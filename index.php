7eb2eb89426e4bc297ea24139ec8d975c825b90c
Mahilin2412 <53659952+Mahilin2412@users.noreply.github.com>
2020-10-06 23:35:25 -0500

    prueba

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-    echo json_encode("Hola");
+    include "servicios/services/Products/DaoProduct.php";
+    include "servicios/services/Products/DaoProduct.php";
+    include "servicios/services/Products/DaoProduct.php";
+    include "servicios/services/Products/DaoProduct.php";
+    include "servicios/services/Products/DaoProduct.php";
+    include "servicios/services/Products/DaoProduct.php";
+
+       
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        if (isset($_GET['apicall'])){
+            switch(isset($_GET['apicall'])){
+                case 'productService':
+                    $model = new ProductDao();
+
+                    $json = $model->getProduct();
+                    echo json_encode($json);
+                break;
+            }
+
+        }
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+    }
 ?>
