0bb1f155e223e6e2668139c3b94e5bb0c2a53efb
Mahilin2412 <53659952+Mahilin2412@users.noreply.github.com>
2020-10-07 00:47:22 -0500

    wqe

--- index.php
+++ index.php
@@ -8,18 +8,20 @@
         if (isset($_GET['apicall'])){
             switch($_GET['apicall']){
                 case 'productService':
-                    include "servicios/services/Products/DaoProduct.php";
+                    /*include "servicios/services/Products/DaoProduct.php";
                     $model = new ProductDao();
 
                     $json = $model->getProduct();
-                    echo json_encode($json);
+                    echo json_encode($json);*/
+		    echo json_encode("Hola");
                 break;
                 case 'categoryProduct':
-                    include "servicios/services/CategoryProduct/DaoCategoryProduct.php";
+                    /*include "servicios/services/CategoryProduct/DaoCategoryProduct.php";
                     $model = new CategoryProdDao();
 
                     $json = $model->getCategoryProduct();
-                    echo json_encode($json);
+                    echo json_encode($json);*/
+		    echo json_encode("Hola2");
                 break;
             }
 
