d1f598039953e22ad7d32dfb2976172407126f19
Juan1007 <48416762+Juan1007@users.noreply.github.com>
2020-10-07 19:54:36 -0500

    Create

---
+++ productService.php
@@ -0,0 +1,76 @@
+<?php
+    include "servicios/services/Products/DaoProduct.php";
+       
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        $model = new ProductDao();
+
+        $json = $model->getProduct();
+        echo json_encode($json);
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $ExisteProduct = false;
+        $type = $_SERVER['CONTENT_TYPE'];
+
+        if($type != 'application/json'){
+            header("http/1.1 500 no es Json");
+        }
+        $content = trim(file_get_contents("php://input"));
+        $datos = json_decode($content,true);
+
+        $model = new ProductDao();
+        $json = $model->PostProduct($datos);
+
+        echo json_encode($json);
+        
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+        //Optenemos el tipo de contenido del llamado del servicio
+        $type = $_SERVER['CONTENT_TYPE'];
+
+        //Siempre debe ser de tipo aplicación Json, de lo contrario enviamos error 
+        if($type != 'application/json'){
+            header("http/1.1 500 no es Json");
+        }
+        //Optenemos el Json y luego lo dejamos en un Array asociativo con la funcion json_decode...
+        //está recibe como parametros el Json y (true o false) este ultimo es para que el array...
+        //sea asociativo o no. 
+        $content = trim(file_get_contents("php://input"));
+        $datos = json_decode($content,true);
+
+        $datosProduct = array(
+            'IdProduct' => $datos['IdProduct'],
+            'NameProduct' => $datos['NameProduct'],
+            'Reference' => $datos['Reference'],
+            'ManIva' => $datos['ManIva'],
+            'PorIva' => $datos['PorIva'],
+            'Price' => $datos['Price'],
+            'StatusProduct' => $datos['StatusProduct'],
+            'Description' => $datos['Description'],
+            'imageProduct' => $datos['imageProduct'],
+            'Clasification' => $datos['Clasification'],
+            'FKIdTypeProduct' => $datos['FKIdTypeProduct'],
+            'FKIdUser' => $datos['FKIdUser'],
+            'Status' => $datos['Status'],
+            'UpdateTimestamp' => $datos['UpdateTimestamp'],
+        );
+        $IdProd = $datos['IdProduct'];
+
+        $model = new ProductDao();
+        $json = $model->UpdateProduct($datosProduct,$IdProd,$datos);
+        echo json_encode($json);
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+        $type = $_SERVER['CONTENT_TYPE'];
+
+        if($type != 'application/json'){
+            header("http/1.1 500 no es Json");
+        }
+        $content = trim(file_get_contents("php://input"));
+        $datos = json_decode($content,true);
+
+        $model = new ProductDao();
+        $json = $model->deleteProduct($datos);
+
+        echo json_encode($json);
+    }
+?>
